8a9052f5d1fc9eb1651914638f6a6cf6792ca711
D M <d.m.michieli@gmail.com>
2026-05-10 17:43:18 +1000

    optimise day 9 part 1 to o(n) operations!

--- cmd/day9/main.go
+++ cmd/day9/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	_ "embed" // Required for the //go:embed directive
 	"fmt"
+	"math"
 	"strconv"
 	"strings"
 )
@@ -42,17 +43,7 @@ func part1() {
 	//           ..............
 	// bottom left             bottom right
 
-	// ............oo
-	// .xxxxxxxxxxxoo
-	// .xxxxxxxxxxxx.
-	// .xxxxxxxxxxxx.
-	// .xxxxxxxxxxxx.
-	// .xxxxxxxxxxxx.
-	// .xxxxxxxxxxxx.
-	// ooxxxxxxxxxxx.
-	// oo............
-
-	// 14, 9
+	// how to determine which point is most bottom left?
 
 	// ..............
 	// ..............
@@ -64,108 +55,75 @@ func part1() {
 	// ..............
 	// ..............
 
-	// No. simply using our ssquare to see if thats closest to the boundary doesn't work, e.g. look at the above
 	// The point at [1, 4] is considered more of a bottom left point than [4,6] by looking at the area of the triangles that get made
-	// with it and the corner.
-	// Maybe instead we need the straght line distance to the corner?
-
-	var topRight location
-	var bottomRight location
-	var topLeft location
-	var bottomLeft location
-
-	// first iterate over all points to establish the size of the boundaries,
-	// and also store all the locations in a slice so that we don't have to recompute these later.
-	xMin := 1000000
-	xMax := 0
-	yMin := 1000000
-	yMax := 0
-
+	// with it and the corner. Instead we need the straght line distance to the corner via pythagoras? Yes this looks good and works!
 	locs := []location{}
 
 	for _, line := range strings.Split(input, "\n") {
 		xy := strings.Split(line, ",")
-
 		x, err1 := strconv.Atoi(xy[0])
 		y, err2 := strconv.Atoi(xy[1])
 		if err1 != nil || err2 != nil {
 			panic(fmt.Sprintf("bad input data, line: '%s'", line))
 		}
-
-		if x < xMin {
-			xMin = x
-		}
-		if x > xMax {
-			xMax = x
-		}
-		if y < yMin {
-			yMin = y
-		}
-		if y > yMax {
-			yMax = y
-		}
-
 		locs = append(locs, location{x: x, y: y})
 	}
 
-	topLeftBoundary := location{x: xMin, y: yMin}
-	topRightBoundary := location{x: xMax, y: yMin}
-	bottomLeftBoundary := location{x: xMin, y: yMax}
-	bottomRightBoundary := location{x: xMax, y: yMax}
+	var topLeft location
+	topLeftBound := location{x: 0, y: 0}
+	topLeftSmallestDist := float64(1000000000000000000)
 
-	topLeftBoundary = location{x: 1000, y: 1000}
-	topRightBoundary = location{x: 100_000, y: 1000}
-	bottomLeftBoundary = location{x: 1000, y: 100_000}
-	bottomRightBoundary = location{x: 100_000, y: 100_000}
+	var topRight location
+	topRightBound := location{x: 100_000, y: 0}
+	topRightSmallestDist := float64(1000000000000000000)
 
-	// Next, for each point, see if it is the closest point to the boundary by seeing which point makes the smallest square area between it and the boundary
-	topRightSmallest := 1000000000000000000
-	bottomRightSmallest := 1000000000000000000
-	topLeftSmallest := 1000000000000000000
-	bottomLeftSmallest := 1000000000000000000
+	var bottomLeft location
+	bottomLeftBound := location{x: 0, y: 100_00}
+	bottomLeftSmallestDist := float64(1000000000000000000)
 
-	for _, loc := range locs {
-		candidate := squareSize(topLeftBoundary, loc)
-		if candidate < topLeftSmallest {
-			topLeftSmallest = candidate
-			topLeft = loc
+	var bottomRight location
+	bottomRightBound := location{x: 100_000, y: 100_000}
+	bottomRightSmallestDist := float64(1000000000000000000)
+
+	var candidate float64
+
+	for _, loc1 := range locs {
+		candidate = pythag(loc1, topLeftBound)
+		if candidate < topLeftSmallestDist {
+			topLeftSmallestDist = candidate
+			topLeft = loc1
 		}
 
-		candidate = squareSize(topRightBoundary, loc)
-		if candidate < topRightSmallest {
-			topRightSmallest = candidate
-			topRight = loc
+		candidate = pythag(loc1, topRightBound)
+		if candidate < topRightSmallestDist {
+			topRightSmallestDist = candidate
+			topRight = loc1
 		}
 
-		candidate = squareSize(bottomLeftBoundary, loc)
-		if candidate < bottomLeftSmallest {
-			bottomLeftSmallest = candidate
-			bottomLeft = loc
+		candidate = pythag(loc1, bottomLeftBound)
+		if candidate < bottomLeftSmallestDist {
+			bottomLeftSmallestDist = candidate
+			bottomLeft = loc1
 		}
 
-		candidate = squareSize(bottomRightBoundary, loc)
-		if candidate < bottomRightSmallest {
-			bottomRightSmallest = candidate
-			bottomRight = loc
+		candidate = pythag(loc1, bottomRightBound)
+		if candidate < bottomRightSmallestDist {
+			bottomRightSmallestDist = candidate
+			bottomRight = loc1
 		}
 	}
 
-	fromTopLeft := squareSize(topLeft, bottomRight)
-	fromBottomLeft := squareSize(topRight, bottomLeft)
-
-	fmt.Println("topLeft:", topLeft)
-	fmt.Println("bottomRight:", bottomRight)
-	fmt.Println("topRight:", topRight)
-	fmt.Println("bottomLeft:", bottomLeft)
-
-	var biggest int
-	if fromBottomLeft > fromBottomLeft {
-		biggest = fromBottomLeft
-	} else {
-		biggest = fromTopLeft
+	biggest := squareSize(topLeft, bottomRight)
+	other := squareSize(topRight, bottomLeft)
+	if other > biggest {
+		biggest = other
 	}
 
 	fmt.Println("part 1:", biggest)
+}
+
+func pythag(loc1, loc2 location) float64 {
+	return math.Sqrt(math.Pow(float64(loc1.x-loc2.x), 2) + math.Pow(float64(loc1.y-loc2.y), 2))
 }
 
 func squareSize(loc1, loc2 location) int {
@@ -184,42 +142,10 @@ func squareSize(loc1, loc2 location) int {
 	return width * height
 }
 
-func part1BruteForce() {
-	locs := []location{}
-	for _, line := range strings.Split(input, "\n") {
-		xy := strings.Split(line, ",")
-		x, err1 := strconv.Atoi(xy[0])
-		y, err2 := strconv.Atoi(xy[1])
-		if err1 != nil || err2 != nil {
-			panic(fmt.Sprintf("bad input data, line: '%s'", line))
-		}
-		locs = append(locs, location{x: x, y: y})
-	}
-
-	var finLoc1 location
-	var finLoc2 location
-
-	largest := 0
-	for _, loc1 := range locs {
-		for _, loc2 := range locs {
-			size := squareSize(loc1, loc2)
-			if size > largest {
-				largest = size
-				finLoc1 = loc1
-				finLoc2 = loc2
-			}
-		}
-	}
-	fmt.Println("part 1 (brute force):", largest)
-	fmt.Println("loc1:", finLoc1)
-	fmt.Println("loc2:", finLoc2)
-}
-
 func part2() {
 }
 
 func main() {
-	part1BruteForce()
 	part1()
 	part2()
 }
